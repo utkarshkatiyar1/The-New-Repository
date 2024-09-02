c1a20dafe0e03dd38933710e3f7c99ccfc3c99d6
janisar007 <134937141+janisar007@users.noreply.github.com>
2024-09-02 23:05:02 +0530

    Update MainChat.jsx

--- src/components/MainChat.jsx
+++ src/components/MainChat.jsx
@@ -1,6 +1,7 @@
 "use client"
 
 import ChatListingItem from "./ChatListingItem";
+import { Input } from "./ui/input";
 
 
 
@@ -9,8 +10,13 @@ export default function MainChat({name, address, price, imagePath}) {
 
 
   return (
-    <div className="bg-[#eaeaea] w-[80%] min-h-screen flex justify-center">
+    <div className="bg-[#eaeaea] w-[80%] min-h-screen flex justify-between flex-col items-center ">
         <ChatListingItem name={name} address={address} price={price} imagePath={imagePath}/>
+        <Input
+                  type="text"
+                  className="border-[1px]  border-gray-600 mb-[5rem]"
+                  
+                />
     </div>
   );
 }
